7b75a4cf7ab55ee42b4336e39356483248cec192
hb9901 <hb9901@gmail.com>
2024-06-03 11:33:41 +0900

    style: comment 컴포넌트 css 추가

--- src/components/Detail/Comment.jsx
+++ src/components/Detail/Comment.jsx
@@ -1,39 +1,77 @@
 import styled from 'styled-components';
+import { getDate } from '../../assets/functions';
 
 function Comment({ writer, date, content, like }) {
+  const dateStr = getDate(date, 'long');
   return (
     <Wrapper>
       <Header>
         <Writer>{writer}</Writer>
         <Info>
           <Like>{like}</Like>
-          <Date>{date}</Date>
+          <Date>{dateStr}</Date>
         </Info>
       </Header>
-      <Body>{content}</Body>
+      <Body>
+        <Content>{content}</Content>
+      </Body>
       <Footer>
         <LikeBtn>Like</LikeBtn>
       </Footer>
+      <HR />
     </Wrapper>
   );
 }
 
 export default Comment;
 
-const Wrapper = styled.li``;
+const Wrapper = styled.li`
+  padding: 10px 5px;
+`;
 
-const Header = styled.div``;
+const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 
-const Writer = styled.div``;
+  margin-bottom: 15px;
+`;
 
-const Info = styled.div``;
+const Writer = styled.div`
+  font-size: 14px;
+`;
 
-const Like = styled.span``;
+const Info = styled.div`
+  display: flex;
+  align-items: end;
+  gap: 10px;
+`;
 
-const Date = styled.span``;
+const Like = styled.span`
+  font-size: 14px;
+`;
 
-const Body = styled.div``;
+const Date = styled.span`
+  font-size: 12px;
+`;
 
-const Footer = styled.div``;
+const Body = styled.div`
+  margin-bottom: 15px;
+`;
+
+const Content = styled.p`
+  font-size: 12px;
+`;
+
+const Footer = styled.div`
+  display: flex;
+  justify-content: end;
+`;
 
 const LikeBtn = styled.button``;
+
+const HR = styled.hr`
+  height: 1px;
+  border: 0;
+  background: rgba(0, 0, 0, 0.1);
+`;

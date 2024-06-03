556795dd7ed2501694b4ae0ea6aba53930ccdb29
RingKim1 <kgh9393@naver.com>
2024-06-03 20:06:16 +0900

    refactor : board, pagenation

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -9,33 +9,33 @@ const Board = () => {
   const { showPost, boardTitle } = usePagenation();
 
   return (
-    <BoardContainer>
+    <StBoardContainer>
       <H2>{boardTitle}</H2>
-      <Postcontainer>
-        <PostInfoWrapper>
+      <StPostcontainer>
+        <StPostInfoWrapper>
           <div>
-            <PostInfo>날짜</PostInfo>
-            <PostInfo>추천수</PostInfo>
-            <PostInfo></PostInfo>
+            <StPostInfo>날짜</StPostInfo>
+            <StPostInfo>추천수</StPostInfo>
+            <StPostInfo></StPostInfo>
           </div>
-          <PostInfo>작성자</PostInfo>
-        </PostInfoWrapper>
+          <StPostInfo>작성자</StPostInfo>
+        </StPostInfoWrapper>
         {showPost}
-      </Postcontainer>
+      </StPostcontainer>
 
-      <ButtonWrapper>
+      <StButtonWrapper>
         <button onClick={() => navigate(`/board/${boardTitle}?page=1`)}>목록</button>
         <Pagenation />
         <button>🖊️글쓰기</button>
-      </ButtonWrapper>
-    </BoardContainer>
+      </StButtonWrapper>
+    </StBoardContainer>
   );
 };
 
 export default Board;
 
 // 스타일드 컴포넌트
-const BoardContainer = styled.div`
+const StBoardContainer = styled.div`
   margin: 0px 5%;
 `;
 
@@ -46,12 +46,12 @@ const H2 = styled.h2`
   margin: 20px 0px;
 `;
 
-const Postcontainer = styled.ul`
+const StPostcontainer = styled.ul`
   display: flex;
   flex-direction: column;
 `;
 
-const PostInfoWrapper = styled.li`
+const StPostInfoWrapper = styled.li`
   background-color: #333;
   color: white;
   padding: 10px 0px;
@@ -62,11 +62,11 @@ const PostInfoWrapper = styled.li`
   justify-content: space-between;
 `;
 
-const PostInfo = styled.span`
+const StPostInfo = styled.span`
   margin: 0px 30px;
 `;
 
-const ButtonWrapper = styled.div`
+const StButtonWrapper = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;

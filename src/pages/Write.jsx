4855cc0d5bf3b05c51ab6d332dbf15a79e4ebf91
hb9901 <hb9901@gmail.com>
2024-06-03 23:09:57 +0900

    update: 게시글 작성 페이지 기본 UI 구현

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,5 +1,16 @@
+import styled from "styled-components";
+import WriteForm from "../components/Write/WriteForm";
+
 function Write() {
-  return <div>Write</div>;
+  return <StMainWrapper>
+    <WriteForm />
+  </StMainWrapper>;
 }
 
 export default Write;
+
+const StMainWrapper = styled.main`
+  margin: 0 auto;
+  max-width: 1200px;
+  padding: 10px;
+`;

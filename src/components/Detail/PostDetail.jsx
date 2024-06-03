6e2505852d9c679bb56477ea648bef164ac26931
hb9901 <hb9901@gmail.com>
2024-06-03 10:26:40 +0900

    style: PostDetail 컴포넌트 css 구현

--- src/components/Detail/PostDetail.jsx
+++ src/components/Detail/PostDetail.jsx
@@ -1,13 +1,17 @@
 import styled from 'styled-components';
 
 function PostDetail() {
+  const separator = `  |  `;
   return (
     <Wrapper>
       <Title>제목</Title>
       <Info>
         <Writer>작성자</Writer>
+        <Separator>{separator}</Separator>
         <Date>날짜</Date>
+        <Separator>{separator}</Separator>
         <ViewNum>조회 수</ViewNum>
+        <Separator>{separator}</Separator>
         <CommentNum>댓글 수</CommentNum>
       </Info>
       <HR />
@@ -22,9 +26,19 @@ export default PostDetail;
 
 const Wrapper = styled.section``;
 
-const Title = styled.h3``;
+const Title = styled.h1`
+  padding-left: 5px;
+  margin-bottom: 15px;
 
-const Info = styled.p``;
+  font-size: 32px;
+  font-weight: 600;
+`;
+
+const Info = styled.p`
+  font-size: 10px;
+`;
+
+const Separator = styled.span``;
 
 const Writer = styled.span``;
 
@@ -34,8 +48,16 @@ const ViewNum = styled.span``;
 
 const CommentNum = styled.span``;
 
-const HR = styled.hr``;
+const HR = styled.hr`
+  height: 1px;
+  border: 0;
+  background: rgba(0, 0, 0, 0.1);
+`;
 
-const Content = styled.div``;
+const Content = styled.div`
+  padding: 30px 0 15px;
+`;
 
-const Article = styled.article``;
+const Article = styled.article`
+  font-size: 13px;
+`;

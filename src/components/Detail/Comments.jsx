3d2c0deb8dc9465a0a672c7cfd92dd73b0ee3a68
hb9901 <hb9901@gmail.com>
2024-06-03 11:11:50 +0900

    style: Comments 컴포넌트 css 추가

--- src/components/Detail/Comments.jsx
+++ src/components/Detail/Comments.jsx
@@ -15,14 +15,14 @@ const mockData = [
     writer: '2',
     content: 'test2',
     date: Date.now(),
-    like: 0,
+    like: 0
   },
   {
     id: nanoid(),
     writer: '3',
     content: 'test3',
     date: Date.now(),
-    like: 0,
+    like: 0
   }
 ];
 
@@ -30,9 +30,13 @@ function Comments() {
   return (
     <Wrapper>
       <TitleWrapper>
+        <TitleImg>
+          <img src="/public/speechBalloon.png" />
+        </TitleImg>
         <Title>댓글</Title>
         <CommentsNum>{mockData.length}</CommentsNum>
       </TitleWrapper>
+      <HR />
       <CommentsList>
         {mockData.map((comment) => (
           <Comment
@@ -52,10 +56,32 @@ export default Comments;
 
 const Wrapper = styled.section``;
 
-const TitleWrapper = styled.div``;
+const TitleWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 30px 0 15px;
+  gap: 3px;
+`;
+
+const TitleImg = styled.div`
+  width: 19px;
+
+  & > img {
+    width: 100%;
+    object-fit: cover;
+  }
+`;
 
 const Title = styled.span``;
 
-const CommentsNum = styled.span``;
+const CommentsNum = styled.span`
+  color: blue;
+`;
+
+const HR = styled.hr`
+  height: 1px;
+  border: 0;
+  background: rgba(0, 0, 0, 0.1);
+`;
 
 const CommentsList = styled.ul``;
